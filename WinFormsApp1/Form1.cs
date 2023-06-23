a3175036f02dddcd209adf5bd6508fe472ce2e1a
Doug Van Auken <douglasvanauken@gmail.com>
2023-06-22 21:13:14 -0500

    init dev

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -4,19 +4,18 @@ using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
+    public class Form1 : Form
     {
         private PictureBox pictureBox;
 
         public Form1()
         {
-            InitializeComponent();
 
             // Create a new PictureBox
             pictureBox = new PictureBox
             {
                 Name = "pictureBox1",
-                Size = new Size(800, 600), // Set the size as needed
+                Size = new Size(300, 200), // Set the size as needed
                 Location = new Point(10, 10), // Set the location as needed
                 BorderStyle = BorderStyle.Fixed3D
             };
