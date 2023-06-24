f7fa0826ab763699faab817a41268832c37d0b40
Doug Van Auken <douglasvanauken@gmail.com>
2023-06-24 13:48:39 -0500

    git commit

--- WinFormsApp1/Rendering/Renderer.cs
+++ WinFormsApp1/Rendering/Renderer.cs
@@ -112,55 +112,54 @@ namespace GeographicProjections.Rendering
         //    return bitmap;
         //}
 
-        //public Bitmap Render(IProjection projection, List<List<Coordinate>> shorelineData)
-        //{
-        //    // Create a new bitmap to draw on
-        //    Bitmap bitmap = new Bitmap(width, height);
-        //    using (Graphics g = Graphics.FromImage(bitmap))
-        //    {
-        //        // Set the background color
-        //        g.Clear(Color.White);
-
-        //        // Draw each geometry
-        //        foreach (var geometry in shorelineData) // <-- Change: Now looping over each geometry
-        //        {
-        //            // Initialize the previous point to null
-        //            PointF? previousPoint = null; // <-- Change: Reset previousPoint for each new geometry
-
-        //            // Draw the shoreline for this geometry
-        //            foreach (var coordinate in geometry)
-        //            {
-        //                // Convert the geographic coordinates to screen coordinates
-        //                Point3D point = projection.Forward(coordinate);
-
-        //                // Scale and translate the point to fit into the PictureBox
-        //                double scaleX = width / (2.0 * Math.PI); // Longitude range is -π to π
-        //                double scaleY = height / Math.PI; // Latitude range is -π/2 to π/2
-        //                double translateX = width / 2.0;
-        //                double translateY = height / 2.0;
-
-        //                // Convert the 3D point to 2D
-        //                PointF point2D = new PointF((float)(point.X * scaleX + translateX), (float)(-point.Y * scaleY + translateY)); // Note the negative sign for y to flip the y-axis
-
-        //                // If there is a previous point, draw a line from the previous point to the current point
-        //                if (previousPoint.HasValue)
-        //                {
-        //                    g.DrawLine(Pens.Black, previousPoint.Value, point2D);
-        //                }
-
-        //                // Update the previous point
-        //                previousPoint = point2D;
-        //            }
-        //        }
-        //    }
-
-        //    return bitmap;
-        //}
-
-        public virtual Bitmap Render(IProjection projection, List<List<Coordinate>> shorelineData)
+        public Bitmap Render(IProjection projection, List<List<Coordinate>> shorelineData)
         {
-            throw new NotImplementedException();
+            // Create a new bitmap to draw on
+            Bitmap bitmap = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                // Set the background color
+                g.Clear(Color.White);
+
+                // Draw each geometry
+                foreach (var geometry in shorelineData) // <-- Change: Now looping over each geometry
+                {
+                    // Initialize the previous point to null
+                    PointF? previousPoint = null; // <-- Change: Reset previousPoint for each new geometry
+
+                    // Draw the shoreline for this geometry
+                    foreach (var coordinate in geometry)
+                    {
+                        // Convert the geographic coordinates to screen coordinates
+                        Point3D point = projection.Forward(coordinate);
+
+                        // Scale and translate the point to fit into the PictureBox
+                        double scaleX = width / (2.0 * Math.PI); // Longitude range is -π to π
+                        double scaleY = height / Math.PI; // Latitude range is -π/2 to π/2
+                        double translateX = width / 2.0;
+                        double translateY = height / 2.0;
+
+                        // Convert the 3D point to 2D
+                        PointF point2D = new PointF((float)(point.X * scaleX + translateX), (float)(-point.Y * scaleY + translateY)); // Note the negative sign for y to flip the y-axis
+
+                        // If there is a previous point, draw a line from the previous point to the current point
+                        if (previousPoint.HasValue)
+                        {
+                            g.DrawLine(Pens.Black, previousPoint.Value, point2D);
+                        }
+
+                        // Update the previous point
+                        previousPoint = point2D;
+                    }
+                }
+            }
+            return bitmap;
         }
+
+        //public virtual Bitmap Render(IProjection projection, List<List<Coordinate>> shorelineData)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
     }
 }
