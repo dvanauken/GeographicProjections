8dea90cd96f02d55772e5100b7217052ef75c17f
Doug Van Auken <douglasvanauken@gmail.com>
2023-06-30 12:00:27 -0500

    init dev

--- WinFormsApp1/Rendering/Polygon.cs
+++ WinFormsApp1/Rendering/Polygon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeographicProjections.Projections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,28 @@ using System.Threading.Tasks;
 
 namespace GeographicProjections.Rendering
 {
+
     public class Polygon
     {
-        public List<Point3D> Points { get; set; }
+        public List<Coordinate> Vertices { get; set; }
 
-        public Polygon(List<Point3D> points)
+        public Polygon()
         {
-            Points = points;
+            Vertices = new List<Coordinate>();
+        }
+
+        public double Area()
+        {
+            // TODO: Implement the calculation of the area of the polygon
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsPoint(Coordinate point)
+        {
+            // TODO: Implement the point-in-polygon algorithm
+            throw new NotImplementedException();
         }
     }
+
+
 }
