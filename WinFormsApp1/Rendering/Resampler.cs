0581c98bfa65ba89615590a502959163605e006c
Doug Van Auken <douglasvanauken@gmail.com>
2023-06-30 11:47:16 -0500

    init dev

--- WinFormsApp1/Rendering/Resampler.cs
+++ WinFormsApp1/Rendering/Resampler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeographicProjections.Projections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,62 @@ using System.Threading.Tasks;
 
 namespace GeographicProjections.Rendering
 {
-    internal class Resampler
+    public class Resampler
     {
+        private double precision; // The desired precision of the resampling
+
+        public Resampler(double precision)
+        {
+            this.precision = precision;
+        }
+
+        public List<Coordinate> Resample(List<Coordinate> coordinates)
+        {
+            List<Coordinate> resampledCoordinates = new List<Coordinate>();
+
+            for (int i = 0; i < coordinates.Count - 1; i++)
+            {
+                Coordinate start = coordinates[i];
+                Coordinate end = coordinates[i + 1];
+
+                // Add the start coordinate to the resampled list
+                resampledCoordinates.Add(start);
+
+                double distance = CalculateDistance(start, end);
+
+                if (distance > precision)
+                {
+                    // Calculate the number of points to insert
+                    int pointsToInsert = (int)(distance / precision);
+
+                    for (int j = 1; j < pointsToInsert; j++)
+                    {
+                        double t = (double)j / pointsToInsert;
+
+                        // Interpolate a new coordinate between start and end
+                        Coordinate interpolated = Interpolate(start, end, t);
+
+                        resampledCoordinates.Add(interpolated);
+                    }
+                }
+            }
+
+            // Add the last coordinate to the resampled list
+            resampledCoordinates.Add(coordinates[coordinates.Count - 1]);
+
+            return resampledCoordinates;
+        }
+
+        private double CalculateDistance(Coordinate a, Coordinate b)
+        {
+            // Implement a method to calculate the distance between two coordinates
+            throw new NotImplementedException();
+        }
+
+        private Coordinate Interpolate(Coordinate a, Coordinate b, double t)
+        {
+            // Implement a method to interpolate a new coordinate between two given coordinates
+            throw new NotImplementedException();
+        }
     }
 }
